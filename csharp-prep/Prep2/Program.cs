af13e1f1ab509b34d083477d2afbb282c9aeface
EMC2001 <emilianocastellanos0@gmail.com>
2026-04-23 18:30:10 -0600

    Prep 1 and 2 Completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What grade did you get (type in a number)? ");
+        string inputGrade = Console.ReadLine();
+        int number = int.Parse(inputGrade);
+
+        string letter = "";
+
+
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (number >= 80)
+        {
+            letter = "B"; 
+        }
+        
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F"; 
+        }
+
+        Console.WriteLine($"Your grade: {letter}");
+
+
+        if (number >=70)
+        {
+            Console.WriteLine("You passed the class"); 
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time");
+        }
+
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        // This is cool
         Console.WriteLine("Hello Sandbox World!");
     }
 }
