adc8fce02f54fed17c822ce860f9a1e9d2c9f07a
EMC2001 <emilianocastellanos0@gmail.com>
2026-05-16 20:28:34 -0600

    Prompts added in Entry

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -11,7 +11,10 @@ class Entry
         "What was the best part of my day?",
         "How did I see the hand of the Lord in my life today?",
         "What was the strongest emotion I felt today?",
-        "If I had one thing I could do over today, what would it be?"
+        "If I had one thing I could do over today, what would it be?",
+        "How can I improve my day tomorrow?",
+        "What is something I am grateful for today?",
+        "How did I improve myself today?"
     };
 
     public void CreateJournalEntry()
