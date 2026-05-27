9afb4b6f8a4855936ccba80483d007688eb1d09c
EMC2001 <emilianocastellanos0@gmail.com>
2026-05-27 14:49:23 -0600

    Word completed

---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,43 @@
+class Word
+{
+    private string _word;
+    private bool _hidden;
+
+    public Word(String word)
+    {
+        _word = word;
+        _hidden = false;
+    }
+
+    public string GetWordString()
+    {
+        string tempWord = "";
+        if (_hidden)
+        {
+            foreach(char c in _word)
+            {
+                tempWord += '_';
+            }
+        }
+        else
+        {
+            tempWord = _word;
+        }
+        return tempWord;
+    }
+
+    public void Hide()
+    {
+        _hidden = true;
+    }
+
+    public bool isHidden()
+    {
+        return _hidden;
+    }
+
+    public void DisplayWord()
+    {
+        Console.WriteLine(GetWordString());
+    }
+}
